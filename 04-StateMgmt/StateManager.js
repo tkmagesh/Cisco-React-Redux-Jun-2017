993868e535103a809e40fdd4523d406ea46ed344
Magesh <tkmagesh77@gmail.com>
2017-06-06 14:03:30 +0530

    BugTracker using State Manager

--- 04-StateMgmt/StateManager.js
+++ 04-StateMgmt/StateManager.js
@@ -1,35 +1,50 @@
 
-
-function createStore(reducer){
-	let _reducer = reducer;
-	let _currentState = undefined;
-	
-	let init_action = { action : '__INIT_ACTION' };
-	_currentState = reducer(_currentState, init_action);
-
-	function getState(){
-		return _currentState;
+var StateManager = (function(){
+	function bindActionCreators(actionCreatorsFn, dispatch){
+		var actions = actionCreatorsFn();
+		var result = {};
+		for(let action in actions){
+			result[action] = function(){
+				dispatch(actions[action].apply(this, arguments));
+			}
+		}
+		return result;
 	}
-	let _subscriptions = [];
+	function createStore(reducer){
+		let _reducer = reducer;
+		let _currentState = undefined;
+		
+		let init_action = { action : '__INIT_ACTION' };
+		_currentState = reducer(_currentState, init_action);
 
-	function subscribe(callback){
-		_subscriptions.push(callback);
-	}
+		function getState(){
+			return _currentState;
+		}
+		let _subscriptions = [];
 
-	function triggerChange(){
-		_subscriptions.forEach(subscription => subscription());
-	}
+		function subscribe(callback){
+			_subscriptions.push(callback);
+		}
 
-	function dispatch(action){
-		var newState = _reducer(_currentState, action);
-		if (newState !== _currentState){
-			_currentState = newState;
-			triggerChange();
+		function triggerChange(){
+			_subscriptions.forEach(subscription => subscription());
+		}
+
+		function dispatch(action){
+			var newState = _reducer(_currentState, action);
+			if (newState !== _currentState){
+				_currentState = newState;
+				triggerChange();
+			}
+		}
+		return {
+			getState : getState,
+			subscribe : subscribe,
+			dispatch : dispatch
 		}
 	}
 	return {
-		getState : getState,
-		subscribe : subscribe,
-		dispatch : dispatch
+		bindActionCreators : bindActionCreators,
+		createStore : createStore
 	}
-}
+})();
